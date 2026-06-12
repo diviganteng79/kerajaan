4a3878612d3e0409a7d19c1169fd69ab2eb66cfe
login_jsc <diviganteng79@gmail.com>
2026-06-12 16:39:37 +0700

    Create firebase-config.js

---
+++ static/firebase-config.js
@@ -0,0 +1,13 @@
+// Isi nilai ini dari Firebase Console > Project settings > Web app.
+// Aplikasi tetap bisa dimainkan offline walaupun konfigurasi masih kosong.
+window.KERAJAAN_FIREBASE_CONFIG = {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+};
+
+// Ganti ID ini kalau ingin punya beberapa room/game berbeda di database yang sama.
+window.KERAJAAN_FIREBASE_DOC_ID = 'default';
